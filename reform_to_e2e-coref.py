8d1d98280fd1a048f507ae89ade3a9405b682885
allen_kuo <allen_kuo@trendmicro.com>
2019-03-04 08:30:17 +0000

    fix cluster format

--- reform_to_e2e-coref.py
+++ reform_to_e2e-coref.py
@@ -15,6 +15,14 @@ import nltk
 nltk.download("punkt")
 from nltk.tokenize import sent_tokenize, word_tokenize
 
+import random
+import string
+from time import time
+
+
+def create_doc_key():
+    return 'nw%s'%(''.join(random.choices(string.ascii_uppercase + string.digits, k=32)))
+
 def preprocess(text):
     raw_sentences = sent_tokenize(text)
     sentences = [word_tokenize(s) for s in raw_sentences]
@@ -46,13 +54,14 @@ def create_cluster(x):
          true_mention = A_mention
     else:
         true_mention = B_mention
-    return (true_mention, Pronoun_mention)
+    return [(true_mention, Pronoun_mention)]
 
 
 # In[3]:
 
 
 def reform_for_e2e_coref(filepath):
+    random.seed(time())
     df = pd.read_csv(filepath, sep='\t')
     
     df['A_mention'] = df[['Text', 'A', 'A-offset']].apply(create_mention_index, axis=1)
@@ -65,7 +74,7 @@ def reform_for_e2e_coref(filepath):
     
     reformed = pd.DataFrame()
     reformed['tmp'] = df['Text'].apply(preprocess)
-    reformed['doc_key'] = 'nw'
+    reformed['doc_key'] = [create_doc_key() for _ in range(reformed.shape[0])]
     reformed['sentences'] = reformed['tmp'].apply(lambda x: x[0])
     reformed['speakers'] = reformed['tmp'].apply(lambda x: x[1])
     reformed['clusters'] = df['clusters']
@@ -78,89 +87,5 @@ def reform_for_e2e_coref(filepath):
 
 
 reform_for_e2e_coref('gap-development.tsv')
-
-
-# In[58]:
-
-
-df = pd.read_csv('gap-development.tsv', sep='\t')
-df.columns
-
-
-# In[59]:
-
-
-df.head()
-
-
-# In[61]:
-
-
-placeholder = 'AAAAAAAAA'
-def create_mention_index(x):
-    try:
-        text, A, offset = x
-
-        A_tok_count = len([_ for _ in word_tokenize(A)])
-        if A_tok_count == 1:
-            A_tok_count = 0
-
-        _text = [c for c in text]
-        _text[offset:offset+len(A)] = placeholder
-        _text = ''.join(_text)
-        _text = [word for sentence in sent_tokenize(_text) for word in word_tokenize(sentence)]
-        start = _text.index(placeholder)
-        return (start, start+A_tok_count)
-    except:
-        return -1
-
-df['A_mention'] = df[['Text', 'A', 'A-offset']].apply(create_mention_index, axis=1)
-df['B_mention'] = df[['Text', 'B', 'B-offset']].apply(create_mention_index, axis=1)
-df['Pronoun_mention'] = df[['Text', 'Pronoun', 'Pronoun-offset']].apply(create_mention_index, axis=1)
-df = df[(df['Pronoun_mention'] != -1) & (df['A_mention'] != -1) & (df['B_mention'] != -1)]
-
-
-# In[69]:
-
-
-def create_cluster(x):
-    A_coref, A_mention, B_coref, B_mention, Pronoun_mention = x
-    true_mention = None
-    if A_coref:
-         true_mention = A_mention
-    else:
-        true_mention = B_mention
-    return (true_mention, Pronoun_mention)
-df['clusters'] = df[['A-coref', 'A_mention', 'B-coref', 'B_mention', 'Pronoun_mention']].apply(create_cluster, axis=1)
-
-
-# In[71]:
-
-
-df['clusters'].head()
-
-
-# In[72]:
-
-
-reformed = pd.DataFrame()
-reformed['tmp'] = df['Text'].apply(preprocess)
-reformed['doc_key'] = 'nw'
-reformed['sentences'] = reformed['tmp'].apply(lambda x: x[0])
-reformed['speakers'] = reformed['tmp'].apply(lambda x: x[1])
-reformed['clusters'] = df['clusters']
-del reformed['tmp']
-reformed.head()
-
-
-# In[78]:
-
-
-reformed.to_json('gap-development.json', orient='records', lines=True)
-
-
-# In[ ]:
-
-
-
-
+reform_for_e2e_coref('gap-validation.tsv')
+reform_for_e2e_coref('gap-test.tsv')
