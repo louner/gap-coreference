8748d783d575cfa6ca0d49a01e4f9e045f5ad083
allen_kuo <allen_kuo@trendmicro.com>
2019-03-08 08:35:06 +0000

    add clusters_index to re-order cluster to pronoun-A-B

--- reform.py
+++ reform.py
@@ -53,12 +53,21 @@ def create_cluster(x):
     A_coref, A_mention, B_coref, B_mention, Pronoun_mention = x
     true_mention = None
     if A_coref:
-        return ((A_mention, Pronoun_mention), [(B_mention)])
+        return ((Pronoun_mention, A_mention), [(B_mention)])
     elif B_coref:
-        return ((B_mention, Pronoun_mention), [(A_mention)])
+        return ((Pronoun_mention, B_mention), [(A_mention)])
     else:
         return ([(Pronoun_mention)], [(B_mention)], [(A_mention)])
 
+def create_cluster_index(x):
+    A_coref, A_mention, B_coref, B_mention, Pronoun_mention = x
+    true_mention = None
+    if A_coref:
+        return (0, 1, 2)
+    elif B_coref:
+        return (0, 2, 1)
+    else:
+        return (0, 2, 1)
 # In[3]:
 
 
@@ -70,12 +79,13 @@ def reform_for_e2e_coref(filepath, training=True):
     df['Pronoun_mention'] = df[['Text', 'Pronoun', 'Pronoun-offset']].apply(create_mention_index, axis=1)
     
     df = df[(df['Pronoun_mention'] != -1) & (df['A_mention'] != -1) & (df['B_mention'] != -1)]
-    
     if training:
         df['clusters'] = df[['A-coref', 'A_mention', 'B-coref', 'B_mention', 'Pronoun_mention']].apply(create_cluster, axis=1)
+        df['clusters_index'] = df[['A-coref', 'A_mention', 'B-coref', 'B_mention', 'Pronoun_mention']].apply(create_cluster_index, axis=1)
         df['label'] = df[['A-coref', 'B-coref']].apply(lambda x: 0 if x[0] == True else 1 if x[1] == True else 2, axis=1)
     else:
         df['clusters'] = [(())]*df.shape[0]
+        df['clusters_index'] = [(())]*df.shape[0]
         df['label'] = [0]*df.shape[0]
     
     reformed = pd.DataFrame()
@@ -90,13 +100,14 @@ def reform_for_e2e_coref(filepath, training=True):
     reformed['sentences'] = reformed['tmp'].apply(lambda x: x[0])
     reformed['speakers'] = reformed['tmp'].apply(lambda x: x[1])
     reformed['clusters'] = df['clusters']
+    reformed['clusters_index'] = df['clusters_index']
     del reformed['tmp']
     
     reformed.to_json(filepath.replace('tsv', 'json'), orient='records', lines=True)
 # In[4]:
 
 
-#reform_for_e2e_coref('gap-development.tsv')
-#reform_for_e2e_coref('gap-validation.tsv')
-#reform_for_e2e_coref('gap-test.tsv')
+reform_for_e2e_coref('gap-development.tsv')
+reform_for_e2e_coref('gap-validation.tsv')
+reform_for_e2e_coref('gap-test.tsv')
 reform_for_e2e_coref('test_stage_1.tsv', training=False)
